3dcbc39c1edefbca8855a5b1a93228431ef69f1b
MakotoShing <makotos1004@outlook.jp>
2019-05-23 07:45:31 +0900

    Fix one part on models.py

--- question/models.py
+++ question/models.py
@@ -22,8 +22,8 @@ class Post(models.Model):
 class Answer(models.Model):
     # user = models.ForeignKey(User, on_delete=models.CASCADE)
     # question = models.ForeignKey(Post, on_delete=models.PROTECT)
-    answer = models.TextField()
     # time = models.DateTimeField(default=timezone.now)
+    answer = models.TextField()
 
     def __str__(self):
         return self.answer
